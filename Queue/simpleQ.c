67c9e936ea7235c748b66fd557e25134d48e76f2
IshaVaidya-16 <isvaidya1603@gmail.com>
2026-05-21 16:33:02 +0530

    simple queue implementation

--- Queue/simpleQ.c
+++ Queue/simpleQ.c
@@ -1,89 +1,89 @@
-#include<stdio.h>
-#include<stdlib.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 #define size 100
+
 int queue[size];
-int rear=-1;
-int front=-1;
+int rear = -1;
+int front = -1;
 void insert(int val)
 {
-    if(rear==size-1)
+    if (rear == size - 1)
     {
         printf("queue overflow");
         return;
     }
-    if(front==-1)
+    if (front == -1)
     {
-        front=0;
+        front = 0;
     }
-        rear++;
-        queue[rear]=val;
-        printf("element inserted");
+    rear++;
+    queue[rear] = val;
+    printf("element inserted");
 }
 
 void dequeue()
 {
-    if(front==-1|| front>rear)
+    if (front == -1 || front > rear)
     {
         printf("queue empty to delete");
         return;
     }
-    printf("%d removed",queue[front]);
+    printf("%d removed", queue[front]);
     front++;
-    if(front>rear)
+    if (front > rear)
     {
-        front=rear-1;
+        front = rear - 1;
     }
 }
 
 void display()
 {
-    if(front==-1)
+    if (front == -1)
     {
         printf("queue empty to delete");
         return;
     }
-    
+
     printf("queue:\n");
-    
-    for(int i=front;i<=rear;i++)
+
+    for (int i = front; i <= rear; i++)
     {
-        printf("%d ",queue[i]);
+        printf("%d ", queue[i]);
     }
 }
 
 int main()
 {
     int val;
-      int ch;
-    do{
-    printf("1)insert\n2)delete\n3)display\n4)exit\n");
-  
-    printf("enter choice:");
-    scanf("%d",&ch);
-    switch(ch)
+    int ch;
+    do
     {
+        printf("1)insert\n2)delete\n3)display\n4)exit\n");
+
+        printf("enter choice:");
+        scanf("%d", &ch);
+        switch (ch)
+        {
         case 1:
-        printf("enter val:");
-        scanf("%d",&val);
-        insert(val);
-        break;
-        
+            printf("enter val:");
+            scanf("%d", &val);
+            insert(val);
+            break;
+
         case 2:
-        dequeue();
-        break;
-        
+            dequeue();
+            break;
+
         case 3:
-        display();
-        break;
+            display();
+            break;
         case 4:
-        exit(0);
-        
+            exit(0);
+
         default:
-        printf("wrong val");
-    }
-    
-}while(ch!=4);
-return 0;
-    
+            printf("wrong val");
+        }
+
+    } while (ch != 4);
+    return 0;
 }
