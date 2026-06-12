bf327636edf00dad879b107ee3eaffaea7581b90
IshaVaidya-16 <isvaidya1603@gmail.com>
2026-06-12 16:55:02 +0530

    Count Sort updated

--- Sorting/countSort.c
+++ Sorting/countSort.c
@@ -35,15 +35,18 @@ void countSort(int arr[],int n)
 {
     int maxEle=findMax(arr,n);
     int newarr[maxEle+1];
+    //initialize newarr
     for(int i=0;i<=maxEle;i++)
     {
         newarr[i]=0;
     }
+    //count occurrences 
     for(int i=0;i<n;i++)
     {
         newarr[arr[i]]++;
     }
     int k=0;
+    //reconstruct the sorted array
     for(int i=0;i<=maxEle;i++)
     {
         while(newarr[i]>0)
